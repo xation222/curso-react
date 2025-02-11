58c9d7e8030ce096ca3ad89a90de6b47ef4a5519
xation222 <126095360+xation222@users.noreply.github.com>
2025-02-11 07:22:23 -0300

    instalação correta

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className='text-3xl text-center'>Título legal</h1>
+    <p className='text-center text-1xl'>lorem</p>
+    </>
+  )
+}
+
+export default App
